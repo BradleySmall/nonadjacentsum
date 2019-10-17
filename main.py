527ad04c318219f8151a1e50471ebbd612c79906
Bradley M. Small <bradley_small@hotmail.com>
2019-10-16 22:48:33 -0400

    adding in the unwound version learn it!

--- main.py
+++ main.py
@@ -15,9 +15,20 @@ def r_sum(the_list):
     return max(the_list[0] + r_sum(the_list[2:]),
                the_list[1] + r_sum(the_list[3:]))
 
+# figure out how this works and internalize it
+def s_sum(nums):
+    curr_max, prev, two_back = 0, 0, 0
+    for value in nums:
+        curr_max = max(two_back + value, prev)
+        prev, two_back = curr_max, prev
+    return curr_max
+
+
 def main():
     """ Main driver """
     print(test_list, r_sum(test_list))
+
+    print(s_sum(test_list))
 
 
 if __name__ == "__main__":
